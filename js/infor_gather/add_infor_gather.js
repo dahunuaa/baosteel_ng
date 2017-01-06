55c0e9c7290fac6840020a8f4f3808ea95f7aaff
dahu <707363509@qq.com>
2017-01-06 16:16:33 +0800

    添加情报搜集上传图片

--- js/infor_gather/add_infor_gather.js
+++ js/infor_gather/add_infor_gather.js
@@ -16,6 +16,72 @@ app.controller('myCtrl',function($scope,$http){
 
     $scope.inforgather = {};
 
+    var img1='';
+    var img2='';
+    var img3='';
+
+    $scope.m1='';
+    $scope.m2='';
+    $scope.m3='';
+    $scope.images=[$scope.m1,$scope.m2,$scope.m3]
+
+    //上传图片
+    $(".uploaddiv").click(function(){
+        selectimgindex = $(".uploaddiv").index(this);
+        $(".uploadimg")[selectimgindex].click();
+    });
+
+    $(".uploadimg").change(function(){
+        console.log(this.files[0]);
+        var myfile = this.files[0];
+        $(".uploaddiv").each(function(index){
+            if(index==selectimgindex){
+                $(this).find('img').attr("src",getURL(myfile));
+                return false;
+            }
+        });
+
+        if( myfile.size > 5*1024*1024 ){  //用size属性判断文件大小不能超过5M
+            dhx_alert( "你上传的文件太大了！" )
+        }else {
+            var reader = new FileReader();
+            reader.readAsDataURL(myfile);
+            reader.onload = function (e) {
+                var res = this.result;
+
+                res = res.split(',')[1];
+                console.log(res.split(','));
+                if (selectimgindex == 0) {
+                    img1 = res;
+                    $scope.m1 =img1;
+                    $scope.images = [$scope.m1,$scope.m2,$scope.m3];
+                } else if (selectimgindex == 1) {
+                    img2 = res;
+                    $scope.m2 =img2;
+                    $scope.images = [$scope.m1,$scope.m2,$scope.m3];
+                } else if(selectimgindex == 2){
+                    img3 = res;
+                    $scope.m3 =img3;
+                    $scope.images = [$scope.m1,$scope.m2,$scope.m3];
+                }
+            }
+        }
+    });
+
+    //get img url
+    function getURL(file) {
+        var url = null ;
+        if (window.createObjectURL!=undefined) { // basic
+            url = window.createObjectURL(file) ;
+        } else if (window.URL!=undefined) { // mozilla(firefox)
+            url = window.URL.createObjectURL(file) ;
+        } else if (window.webkitURL!=undefined) { // webkit or chrome
+            url = window.webkitURL.createObjectURL(file) ;
+        }
+        return url ;
+    }
+
+
     $scope.submit = function(){
         if ($scope.inforgather.title==""||$scope.inforgather.title==undefined||$scope.inforgather.title==null){
             dhx_alert("请填写标题！")
@@ -28,6 +94,7 @@ app.controller('myCtrl',function($scope,$http){
         }else if($scope.inforgather.text==""||$scope.inforgather.text==undefined||$scope.inforgather.text==null){
             dhx_alert("请填写正文！")
         }else{
+            dhx_alert(JSON.stringify($scope.images))
             $http({
                 method:'post',
                 url:basePath+"api/v1.0/inforgather",
@@ -38,6 +105,7 @@ app.controller('myCtrl',function($scope,$http){
                     "gather_area":$scope.inforgather.area.text,
                     "gather_oilfield":$scope.inforgather.oilfield,
                     "gather_text":$scope.inforgather.text,
+                    "images_list":JSON.stringify($scope.images)
                 }
             }).success(function(res){
                 if(res.response.success==1){
